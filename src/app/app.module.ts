d52c8271f1fd529c009c73cfb78c19455bf598fa
Carlos Castillo <castillolj3004@g|mail.com>
2022-12-11 20:03:10 -0500

    Login finalizado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminComponent } from './admin/admin.component';
+import { BannerComponent } from './banner/banner.component';
+import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    BannerComponent,
+    FooterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
